cccb00299b0b57d8bc3feae96d328967cfe6d74d
HungFai Koo <jhk888@yahoo.com>
2022-10-19 14:10:34 -0700

    completed another logic for dice game

--- DiceJack.java
+++ DiceJack.java
@@ -1,12 +1,36 @@
+import java.util.Scanner;
+
 public class DiceJack {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         int roll1 = rollDice();
         int roll2 = rollDice();
         int roll3 = rollDice();
 
-        System.out.println(roll1);
-        System.out.println(roll2);
-        System.out.println(roll3);
+        System.out.println("Enter three numbers between 1 and 6");
+
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        int num3 = scan.nextInt();
+
+        if(num1 < 1 || num2 < 1 || num3 < 1){
+            System.out.println("Numbers cannot be less than 1. Shutting game down");
+            System.exit(0);
+        }
+
+        if(num1 > 6 || num2 > 6 || num3 > 6){
+            System.out.println("Numbers cannot be greater than 6. Shutting game down");
+            System.exit(0);
+        }
+
+        int sumOfNumbers = num1+num2+num3;
+        int sumDiceRolls = roll1+roll2+roll3;
+        System.out.println("Dice sum = " + sumDiceRolls + ". Number sum = " + sumOfNumbers);
+
+        checkWin(sumDiceRolls, sumOfNumbers);
+
+
+        scan.close();
     }
 
     public static int rollDice(){
@@ -14,4 +38,13 @@ public class DiceJack {
         randomNumber += 1;
         return (int)randomNumber;
     }
+
+    public static void checkWin(int sumDiceRolls, int sumOfNumbers){
+        if(sumOfNumbers > sumDiceRolls && sumOfNumbers - sumDiceRolls < 3){
+            System.out.println("Congrats, you win!");
+        } else {
+            System.out.println("Sorry! You lose.");
+        }
+    }
+    
 }
